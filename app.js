d02799711f8ceb538b80a8d8dc7a90101bc29daf
codesmitty <varcodesmitty@gmail.com>
2020-11-14 13:19:37 -0500

    filtering data on input change

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const dataContainer = document.getElementById("data-container");
 const api_url = "https://restcountries.eu/rest/v2/all";
+const search_bar = document.getElementById("search-bar");
 
 const getCountry = async (url) => {
   const response = await fetch(url)
@@ -9,51 +10,90 @@ const getCountry = async (url) => {
   return response;
 };
 
-const createElement = (type, className, data, alt) => {
+const createCountriesElement = (type, className, data, alt) => {
   var element = document.createElement(type);
 
-  if(type === "img"){
-      element.src = data;
-      element.alt = alt;
-      element.classList = className;
-      return element;
-  }else{
+  if (type === "img") {
+    element.src = data;
+    element.alt = alt;
+    element.classList = className;
+    return element;
+  } else {
     element.classList = className;
     element.innerHTML = data;
     return element;
   }
+};
 
+const createdCountries = (count) => {
+  const countryBox = createCountriesElement("div", "country-box", "");
+  const countryData = createCountriesElement("div", "country-data", "");
+  const name = createCountriesElement("h1", "country-title", count.name);
+  const population = createCountriesElement(
+    "p",
+    "population",
+    `<span style="font-weight:800">Population:</span> ${count.population}`
+  );
+  const region = createCountriesElement(
+    "p",
+    "region",
+    `<span style="font-weight:800">Region:</span> ${count.region}`
+  );
+  const capital = createCountriesElement(
+    "p",
+    "capital",
+    `<span style="font-weight:800">Capital:</span> ${count.capital}`
+  );
+  const flag = createCountriesElement(
+    "img",
+    "flag",
+    count.flag,
+    "image of flag"
+  );
+
+  countryBox.appendChild(countryData);
+
+  countryData.appendChild(flag);
+  countryData.appendChild(name);
+  countryData.appendChild(population);
+  countryData.appendChild(region);
+  countryData.appendChild(capital);
+
+  dataContainer.appendChild(countryBox);
+};
+
+const renderCountry = async (country, region) => {
+const fetchedCountries = await getCountry(api_url)
+  if(country){
+createdCountries(country)
+
+  }
   
 };
 
-const renderCountry = async (country) => {
-  const fetchCountries = await getCountry(api_url);
-
-  fetchCountries.forEach((count) => {
-      console.log(count)
-    const countryBox = createElement("div", "country-box", "");
-    const countryData = createElement("div", "country-data", "");
-    const name = createElement("h1", "country-title", count.name);
-    const population = createElement(
-      "p",
-      "population",
-      `Population: ${count.population}`
-    );
-    const region = createElement("p", "region", `Region: ${count.region}`);
-    const capital = createElement("p", "capital", `Capital: ${count.capital}`);
-    const flag = createElement('img', 'flag', count.flag, 'image of flag')
-
-    countryBox.appendChild(countryData);
-
-    countryData.appendChild(flag);
-    countryData.appendChild(name);
-    countryData.appendChild(population);
-    countryData.appendChild(region);
-    countryData.appendChild(capital);
-
-    dataContainer.appendChild(countryBox);
+const filterCountries = (typedData, fetchedData) => {
+  return fetchedData.filter((data) => {
+    console.log(typedData)
+    return data.name.match(new RegExp(`^${typedData}`, "i"));
   });
 };
 
-const countries = renderCountry("Mexico");
+search_bar.addEventListener("input", async (e) => {
+  const typedData = e.target.value;
 
+  const fetchedData = await getCountry(api_url);
+  const filteredCountries = filterCountries(typedData, fetchedData);
+
+  filteredCountries.filter(country =>{
+  renderCountry(country)
+  })
+
+
+
+
+ 
+});
+
+
+
+const countries = renderCountry();
