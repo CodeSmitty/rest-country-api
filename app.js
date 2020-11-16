85a1ae37906bf3bcaee26ea9f22c7994f2db41e1
codesmitty <varcodesmitty@gmail.com>
2020-11-16 18:11:58 -0500

    got the filter by region to work

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const dataContainer = document.getElementById("data-container");
+let dataContainer = document.getElementById("data-container");
 const api_url = "https://restcountries.eu/rest/v2/all";
 const search_bar = document.getElementById("search-bar");
+const dropDown = document.querySelector("select");
 
 const getCountry = async (url) => {
   const response = await fetch(url)
@@ -58,42 +59,122 @@ const createdCountries = (count) => {
   countryData.appendChild(population);
   countryData.appendChild(region);
   countryData.appendChild(capital);
+  return `
+    <div class='country-box'>
+      <div class='country-data'>
+      <img class='flag' src=${count.flag} />
+        <h1 class='country-title'>${count.name}</h1>
+        <p class='population'><b>Population</b> ${count.population}</p>
+        <p class='region'><b>Region</b> ${count.region}</p>
+        <p class='capital'><b>Capital:</b> ${count.capital}</p>
+        
+      </div>
+    </div>
+`;
+};
 
-  dataContainer.appendChild(countryBox);
+const createRegionCountries = (count) => {
+  const countryBox = createCountriesElement("div", "country-box", "");
+  const countryData = createCountriesElement("div", "country-data", "");
+  const name = createCountriesElement("h1", "country-title", count.name);
+  const population = createCountriesElement(
+    "p",
+    "population",
+    `<span style="font-weight:800">Population:</span> ${count.population}`
+  );
+  const region = createCountriesElement(
+    "p",
+    "region",
+    `<span style="font-weight:800">Region:</span> ${count.region}`
+  );
+  const capital = createCountriesElement(
+    "p",
+    "capital",
+    `<span style="font-weight:800">Capital:</span> ${count.capital}`
+  );
+  const flag = createCountriesElement(
+    "img",
+    "flag",
+    count.flag,
+    "image of flag"
+  );
+
+  countryBox.appendChild(countryData);
+
+  countryData.appendChild(flag);
+  countryData.appendChild(name);
+  countryData.appendChild(population);
+  countryData.appendChild(region);
+  countryData.appendChild(capital);
+  return countryBox;
 };
 
 const renderCountry = async (country, region) => {
-const fetchedCountries = await getCountry(api_url)
-  if(country){
-createdCountries(country)
+  const fetchedCountries = await getCountry(api_url);
 
-  }
-  
+  return fetchedCountries.map((count) => {
+    let newCountry = createdCountries(count);
+    dataContainer.innerHTML += newCountry;
+  });
 };
 
 const filterCountries = (typedData, fetchedData) => {
   return fetchedData.filter((data) => {
-    console.log(typedData)
-    return data.name.match(new RegExp(`^${typedData}`, "i"));
+    return data.name.toLowerCase().includes(typedData.toLowerCase());
   });
 };
 
 search_bar.addEventListener("input", async (e) => {
+  const data = await fetch(api_url);
+  const fetchedData = await data.json();
   const typedData = e.target.value;
 
-  const fetchedData = await getCountry(api_url);
-  const filteredCountries = filterCountries(typedData, fetchedData);
+  const filteredCountries = await filterCountries(typedData, fetchedData);
 
-  filteredCountries.filter(country =>{
-  renderCountry(country)
-  })
+  if(typedData == ""){
+    console.log('hello')
+    dataContainer.innerHTML = ""
+    renderCountry()
+  }else if(typedData){
+    dataContainer.innerHTML = ""
+    filteredCountries.map((country) => {
+      let newCountry = createdCountries(country);
+      dataContainer.innerHTML += newCountry;
+    });
+  }
 
 
-
-
- 
 });
 
+const filterByRegion = (selectedData, fetchedData) => {
+  return fetchedData.filter((data) => {
+    return data.region.includes(selectedData);
+  });
+};
 
+dropDown.addEventListener("change", async (e) => {
+  const data = await fetch(api_url);
+  const fetchedData = await data.json();
+  const typedData = e.target.value;
 
-const countries = renderCountry();
+  console.log(typedData);
+
+  const filteredRegions = await filterByRegion(typedData, fetchedData);
+
+  if(typedData == ""){
+    console.log('hello')
+    dataContainer.innerHTML = "";
+    renderCountry()
+  }else{
+    dataContainer.innerHTML = ""
+    filteredRegions.map((item) => {
+      const regions = createdCountries(item);
+      
+      dataContainer.innerHTML += regions;
+    });
+  }
+  
+});
+
+renderCountry()
+
